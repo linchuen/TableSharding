8c2634c27ab1a1e6d401e1c1f818bb9fb587dd6c
Aiden <aiden.lin@ambernet.com.tw>
2024-02-29 17:29:38 +0800

    add test data

--- src/main/resources/sql/country_record.sql
+++ src/main/resources/sql/country_record.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE `country_record_taiwan` (
+CREATE IGNORE TABLE `country_record_taiwan` (
   `id` bigint(20) NOT NULL,
   `country_id` int(11) NOT NULL,
   `stock` varchar(20) DEFAULT NULL,
@@ -8,7 +8,14 @@ CREATE TABLE `country_record_taiwan` (
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `country_record_china` (
+INSERT IGNORE INTO main.country_record_taiwan
+(id, country_id, stock, price, updated_time)
+VALUES
+(1, 1, 'taiwan01', 100, now()),
+(2, 1, 'taiwan02', 200, now()),
+(3, 1, 'taiwan03', 300, now());
+
+CREATE IGNORE TABLE `country_record_china` (
   `id` bigint(20) NOT NULL,
   `country_id` int(11) NOT NULL,
   `stock` varchar(20) DEFAULT NULL,
@@ -17,7 +24,14 @@ CREATE TABLE `country_record_china` (
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `country_record_japan` (
+INSERT IGNORE INTO main.country_record_china
+(id, country_id, stock, price, updated_time)
+VALUES
+(4, 2, 'china01', 100, now()),
+(5, 2, 'china02', 200, now()),
+(6, 2, 'china03', 300, now());
+
+CREATE IGNORE TABLE `country_record_japan` (
   `id` bigint(20) NOT NULL,
   `country_id` int(11) NOT NULL,
   `stock` varchar(20) DEFAULT NULL,
@@ -26,7 +40,14 @@ CREATE TABLE `country_record_japan` (
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `country_record_america` (
+INSERT IGNORE INTO main.country_record_japan
+(id, country_id, stock, price, updated_time)
+VALUES
+(7, 3, 'japan01', 100, now()),
+(8, 3, 'japan02', 200, now()),
+(9, 3, 'japan03', 300, now());
+
+CREATE IGNORE TABLE `country_record_america` (
   `id` bigint(20) NOT NULL,
   `country_id` int(11) NOT NULL,
   `stock` varchar(20) DEFAULT NULL,
@@ -34,3 +55,10 @@ CREATE TABLE `country_record_america` (
   `updated_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
+
+INSERT IGNORE INTO main.country_record_america
+(id, country_id, stock, price, updated_time)
+VALUES
+(10, 4, 'america01', 100, now()),
+(11, 4, 'america02', 200, now()),
+(12, 4, 'america03', 300, now());
